af93a25a38703439a4241724ba173bf44f49a13f
Lamine Kaba <Lkaba2003@yahoo.com>
2014-11-23 00:45:44 -0600

    changed the setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
 	:address					=> 'smtp.sendgrid.net',
-	:port						=> '587',
+	:port						=> '25',
 	:authentication				=> :plain,
 	:user_name					=> 'app31869246@heroku.com',
 	:password					=> '2irrrvzr',
